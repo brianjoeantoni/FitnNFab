bcedc057e4fe396323883858ecf663c548efc113
Brian Joe Antoni <brianjoeantoni@gmail.com>
2023-11-03 11:18:54 +0700

    detailClass + modal complete

--- src/home.js
+++ src/home.js
@@ -1,7 +1,6 @@
 const balanceEye = document.getElementById("balance-eye");
 const balance = document.getElementById("member-balance");
 
-
 // toggle eye icon Home
 function changeImageHome() {
   if (balanceEye.getAttribute("src") === "../assets/Login/visible.png") {
@@ -19,4 +18,22 @@ function hideBalance() {
   }
 }
 
-// toggle active date
+// modal 1
+const openConfirmationButton = document.querySelector(
+  "[data-open-confirmation-modal]"
+);
+const closeNoButton = document.querySelector("[data-no-close-modal]");
+const closeYesButton = document.querySelector("[data-yes-close-modal]");
+const modalConfirmation = document.querySelector("[data-confirmation-modal]");
+
+openConfirmationButton.addEventListener("click", () => {
+  modalConfirmation.showModal();
+});
+
+closeNoButton.addEventListener("click", () => {
+  modalConfirmation.close();
+});
+
+closeYesButton.addEventListener("click", () => {
+  modalConfirmation.close();
+});
